72d1224ae4d4df11920f1b089dd22ae3369ecf17
OHshajim <ajshajimmax@gmail.com>
2024-04-05 01:58:20 +0600

    news details add

--- src/pages/News/NewsDetails.jsx
+++ src/pages/News/NewsDetails.jsx
@@ -1,13 +1,33 @@
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import Header from "../Shared/Header/Header";
 import RightSideNav from "../Shared/RightSide/RightSideNav";
+import { AiOutlineArrowLeft } from "react-icons/ai";
 
 const NewsDetails = () => {
+    const Id = useParams()
+    const News = useLoaderData()
+    // console.log(Id._id, News);
+    const CurrentNews = News.filter(news => news._id === Id._id)
+    console.log(CurrentNews[0]);
+    const { details, title, image_url } = CurrentNews[0]
     return (
         <div className="max-w-[1700px] mx-auto ">
             <Header />
-            <div className="grid grid-cols-4">
+            <div className="grid md:grid-cols-4 gap-5">
                 <div className="col-span-3">
                     <h3 className="text-xl font-semibold mb-6">Dragon News Home</h3>
+                    <div className="p-8 border border-[#e7e7e77d] rounded-lg">
+                        <img src={image_url} alt="" className="w-full " />
+                        <h3 className="text-2xl font-bold mt-5 mb-2">{title}</h3>
+                        <p className="text-[#706F6F]">{details}</p>
+
+                        <Link to="/">
+                            <button
+                                className="btn bg-[#D72050] rounded-none text-white border-none text-lg font-medium px-6 mt-8"
+                            ><AiOutlineArrowLeft /> All news in this category</button>
+                        </Link>
+
+                    </div>
 
                 </div>
                 <div>
